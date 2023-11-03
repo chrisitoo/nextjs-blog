132e4045acc40f97e92d14ff2450846c631e9c71
Chris <Chrisito1127@gmail.com>
2023-11-03 16:48:14 -0400

    Add initial CSS

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -2,21 +2,20 @@ import Link from 'next/link';
 
 import Head from 'next/head';
 
-
-
+import Layout from '../../components/layout';
 
 export default function FirstPost(){
     return (
-        <>
-        <Head>
-            <title> First Post </title>
-        </Head>
+        <Layout>
+            <Head>
+                <title> First Post </title>
+            </Head>
 
         
-            <h1> First Post </h1>
-            <h2>
+                <h1> First Post </h1>
+                <h2>
                 <Link href ="/"> Back to home </Link>
-            </h2>
-        </>
-    )
+                </h2>
+        </Layout>
+    );
 }
